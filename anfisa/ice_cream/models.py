59fb37d7b7f426b403e90dce740704c2198d53db
kapt0xa <kuzmich.ml@gmail.com>
2025-01-20 08:53:37 +0300

    ice_cream: models.py: create model for ice cream

--- anfisa/ice_cream/models.py
+++ anfisa/ice_cream/models.py
@@ -2,9 +2,29 @@ from django.db import models
 
 
 # Топпинги
+class Topping(models.Model):
+    title = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True, max_length=64)
+    is_published = models.BooleanField(default=True)
 
 
 # Обёртки
+class Wrapper(models.Model):
+    title = models.CharField(max_length=256)
+    is_published = models.BooleanField(default=True)
 
 
 # Сорта мороженого
+class IceCream(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.TextField(blank=False)
+    is_on_main = models.BooleanField(default=False)
+    is_published = models.BooleanField(default=True)
+
+
+# Категории
+class Category(models.Model):
+    title = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True, max_length=64)
+    output_order = models.PositiveSmallIntegerField(default=100)
+    is_published = models.BooleanField(default=True)
