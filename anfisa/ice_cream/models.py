d0ea774f8299fa6b7005dfee28baa565308b8e56
kapt0xa <kuzmich.ml@gmail.com>
2025-01-20 09:41:18 +0300

    Спринт 8/27: Тема 2/6: pt.1: passed

--- anfisa/ice_cream/models.py
+++ anfisa/ice_cream/models.py
@@ -1,33 +1,35 @@
 from django.db import models
 
 
-# Топпинги
-class Topping(models.Model):
-    title = models.CharField(max_length=256)
-    slug = models.SlugField(unique=True, max_length=64)
-    is_published = models.BooleanField(default=True)
-
-
-# Обёртки
-class Wrapper(models.Model):
-    title = models.CharField(max_length=256)
-    is_published = models.BooleanField(default=True)
-
-
-# Сорта мороженого
-class IceCream(models.Model):
-    title = models.CharField(max_length=256)
-    description = models.TextField(blank=False)
-    is_on_main = models.BooleanField(default=False)
-    is_published = models.BooleanField(default=True)
-    category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    toppings = models.ManyToManyField('Topping', blank=False)
-    wrapper = models.OneToOneField('Wrapper', on_delete=models.SET_NULL)
-
-
-# Категории
+# Категории.
 class Category(models.Model):
-    title = models.CharField(max_length=256)
-    slug = models.SlugField(unique=True, max_length=64)
-    output_order = models.PositiveSmallIntegerField(default=100)
     is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=64, unique=True)
+    output_order = models.PositiveSmallIntegerField(default=100)
+
+
+# Топпинги.
+class Topping(models.Model):
+    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=64, unique=True)
+
+
+# Обёртки.
+class Wrapper(models.Model):
+    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=256)
+
+
+# Сорта мороженого.
+class IceCream(models.Model):
+    is_published = models.BooleanField(default=True)
+    is_on_main = models.BooleanField(default=False)
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    # Создайте нужные связи между моделями:
+    wrapper = models.OneToOneField(Wrapper, on_delete=models.SET_NULL,
+                                   blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    toppings = models.ManyToManyField(Topping, blank=False)
