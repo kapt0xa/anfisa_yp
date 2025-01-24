daf93cac24b089243109aed1ca714bc283396e64
kapt0xa <kuzmich.ml@gmail.com>
2025-01-24 03:05:10 +0300

    re faking fresh

--- anfisa/core/models.py
+++ anfisa/core/models.py
@@ -2,15 +2,8 @@ from django.db import models
 
 
 class PublishedModel(models.Model):
-<<<<<<< HEAD
     """Абстрактная модель. Добвляет флаг is_published."""
     is_published = models.BooleanField(default=True)
 
     class Meta:
         abstract = True
-=======
-    is_published = models.BooleanField(default=True)
-
-    class Meta:
-        abstract = True
->>>>>>> b5b3d533ad28d280a2ed2a7679527113b8de704b
--- anfisa/ice_cream/models.py
+++ anfisa/ice_cream/models.py
@@ -2,44 +2,24 @@ from django.db import models
 
 from core.models import PublishedModel
 
-<<<<<<< HEAD
 
-=======
-from core.models import PublishedModel
-
-
-# Категории.
->>>>>>> b5b3d533ad28d280a2ed2a7679527113b8de704b
 class Category(PublishedModel):
     title = models.CharField(max_length=256)
     slug = models.SlugField(max_length=64, unique=True)
     output_order = models.PositiveSmallIntegerField(default=100)
 
 
-<<<<<<< HEAD
-=======
-# Топпинги.
->>>>>>> b5b3d533ad28d280a2ed2a7679527113b8de704b
 class Topping(PublishedModel):
     title = models.CharField(max_length=256)
     slug = models.SlugField(max_length=64, unique=True)
 
 
-<<<<<<< HEAD
 class Wrapper(PublishedModel):
     title = models.CharField(
         max_length=256
     )
 
 
-=======
-# Обёртки.
-class Wrapper(PublishedModel):
-    title = models.CharField(max_length=256)
-
-
-# Сорта мороженого.
->>>>>>> b5b3d533ad28d280a2ed2a7679527113b8de704b
 class IceCream(PublishedModel):
     is_on_main = models.BooleanField(default=False)
     title = models.CharField(max_length=256)
