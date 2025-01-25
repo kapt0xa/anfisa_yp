db3a00ed4d7eb55e825dda44a676f71a4aca0083
kapt0xa <kuzmich.ml@gmail.com>
2025-01-25 03:58:59 +0300

    emm...

--- anfisa/ice_cream/admin.py
+++ anfisa/ice_cream/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, IceCream, Wrapper, Topping
 
 
 admin.site.register(Category)
+admin.site.register(IceCream)
+admin.site.register(Wrapper)
+admin.site.register(Topping)
