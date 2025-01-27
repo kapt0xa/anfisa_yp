b99a0a5b302d615ca3d54bc4c71162d3aa0e3f6b
kapt0xa <kuzmich.ml@gmail.com>
2025-01-27 03:35:40 +0300

    scheiße

--- anfisa/ice_cream/models.py
+++ anfisa/ice_cream/models.py
@@ -4,33 +4,35 @@ from core.models import PublishedModel
 
 
 class Category(PublishedModel):
-    title = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=64, unique=True)
-    output_order = models.PositiveSmallIntegerField(default=100)
+    title = models.CharField(max_length=256, verbose_name='Название')
+    slug = models.SlugField(max_length=64, unique=True, verbose_name='Слаг')
+    output_order = models.PositiveSmallIntegerField(default=100, verbose_name='Порядок вывода')
 
     class Meta:
         verbose_name = 'Категория'
 
 
 class Topping(PublishedModel):
-    title = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=64, unique=True)
+    title = models.CharField(max_length=256, verbose_name='Название')
+    slug = models.SlugField(max_length=64, unique=True, verbose_name='Слаг')
 
     class Meta:
-        verbose_name = 'Топпинги'
+        verbose_name = 'Топпинг'
+        verbose_name_plural = 'Топпинги'
 
 
 class Wrapper(PublishedModel):
-    title = models.CharField(max_length=256)
+    title = models.CharField(max_length=256, verbose_name='Название')
 
     class Meta:
-        verbose_name = 'Обёртки'
+        verbose_name = 'Обёртка'
+        verbose_name_plural = 'Обёртки'
 
 
 class IceCream(PublishedModel):
-    is_on_main = models.BooleanField(default=False)
-    title = models.CharField(max_length=256)
-    description = models.TextField()
+    is_on_main = models.BooleanField(default=False, verbose_name='На главной')
+    title = models.CharField(max_length=256, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
     wrapper = models.OneToOneField(
         Wrapper,
         on_delete=models.SET_NULL,
