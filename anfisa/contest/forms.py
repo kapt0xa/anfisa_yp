9d11fad617ed9e8be5294a3daec4fc3e2aad5ce9
kapt0xa <kuzmich.ml@gmail.com>
2025-02-26 10:28:49 +0300

    refresh

--- anfisa/contest/forms.py
+++ anfisa/contest/forms.py
@@ -1,7 +1,19 @@
 from django import forms
 
+
 class ContestForm(forms.Form):
-    title = forms.CharField(max_length=20, label='Название')
-    description = forms.CharField(widget=forms.Textarea, label='Описание')
-    price = forms.DecimalField(min_value=10, max_value=100, help_text='Рекомендованная розничная цена', label='Цена')
-    comment = forms.CharField(widget=forms.Textarea, required=False, label='Комментарий')
+    title = forms.CharField(label='Название', max_length=20)
+    description = forms.CharField(
+        label='Описание',
+        widget=forms.Textarea({'cols': '22', 'rows': '5'}),
+    )
+    price = forms.IntegerField(
+        label='Цена',
+        min_value=10, max_value=100,
+        help_text='Рекомендованная розничная цена',
+    )
+    comment = forms.CharField(
+        label='Комментарий',
+        required=False,
+        widget=forms.Textarea({'cols': '22', 'rows': '5'}),
+    )
--- anfisa/contest/views.py
+++ anfisa/contest/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
 
-from .forms import ContestForm
 
-
-def proposal_create(request):
-    form = ContestForm()
-    return render(request, 'contest/form.html', {'form': form})
-
-
-def accepted(request):
-    return render(request, 'contest/proposal_accepted.html')
+def proposal(request):
+    return render(request, 'contest/form.html')
