5a47c6b210ce2b217a2896ac954a7412a63f142c
kapt0xa <kuzmich.ml@gmail.com>
2025-02-27 11:01:15 +0300

    not working

--- anfisa/contest/views.py
+++ anfisa/contest/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from .forms import ContestForm
+
 
 def proposal(request):
-    return render(request, 'contest/form.html')
+    context = {'form': ContestForm()}
+    if request.GET:
+        form = ContestForm(request.GET)
+        if form.is_valid():
+            context['form'] = form
+
+    return render(request, 'contest/form.html', context)
