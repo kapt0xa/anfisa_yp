eb4d53c56f3c596a6da4a803a909a1b66505188f
kapt0xa <kuzmich.ml@gmail.com>
2025-02-27 11:07:05 +0300

    works, not passed

--- anfisa/contest/views.py
+++ anfisa/contest/views.py
@@ -5,9 +5,8 @@ from .forms import ContestForm
 
 def proposal(request):
     context = {'form': ContestForm()}
-    if request.GET:
-        form = ContestForm(request.GET)
-        if form.is_valid():
-            context['form'] = form
+    form = ContestForm(request.GET or None)
+    if form.is_valid():
+        context['form'] = form
 
     return render(request, 'contest/form.html', context)
